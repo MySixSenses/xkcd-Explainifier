58832ad0f93b823989ad838faf226b858528f963
MySixSenses <ryannotryan1190@gmail.com>
2020-08-28 16:27:20 -0700

    Reduce amount of code, added mobile support

--- chromium/popup/popup.js
+++ chromium/popup/popup.js
@@ -1,48 +1,29 @@
 const nopinexplain = document.getElementById("nopinexplain");
 const pinexplain = document.getElementById("pinexplain");
+function QueryProperTab(Pin){
+    chrome.tabs.query({active: true,currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        if (url.startsWith('https://')){
+            if (url.startsWith('https://m.')){
+                url = url.substring(9)
+            } else{
+                url = url.substring(8)
+            }
+        } else if(url.startsWith('http://')){
+            url = url.substring(7)
+        }
+        url  = "explain" + url    
+        chrome.tabs.create({"url": url, "pinned": Pin})
+    });
+}
 if (nopinexplain) {
     nopinexplain.onclick = function() {
-        chrome.tabs.query({active: true,currentWindow: true}, function(tabs) {
-            var tab = tabs[0];
-            var url = tab.url;
-            var IsHttps = true;
-            if (url.startsWith('https://')){
-                url = url.substring(8)
-            } else if(url.startsWith('http://')){
-                IsHttps = false;
-                url = url.substring(7)
-            }
-            if (IsHttps){
-                url  = "explain" + url
-                url = "https://" + url
-            } else{
-                url  = "explain" + url
-                url = "http://" + url
-            }
-            chrome.tabs.create({"url": url})
-        });
+        QueryProperTab(false);
     }
 }
 if (pinexplain){
     pinexplain.onclick = function() {
-        chrome.tabs.query({active: true,currentWindow: true}, function(tabs) {
-            var tab = tabs[0];
-            var url = tab.url;
-            var IsHttps = true;
-            if (url.startsWith('https://')){
-                url = url.substring(8)
-            } else if(url.startsWith('http://')){
-                IsHttps = false;
-                url = url.substring(7)
-            }
-            if (IsHttps){
-                url  = "explain" + url
-                url = "https://" + url
-            } else{
-                url  = "explain" + url
-                url = "http://" + url
-            }
-            chrome.tabs.create({"url": url, "pinned": true})
-        });
+        QueryProperTab(true);
     }
 }
